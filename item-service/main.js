6da26335539a93d55772752269995c6f268becb4
Krunoslav Banovac <krunoslav@martian.agency>
2019-04-01 03:07:59 +0200

    made persistant change from within docker-compose

--- item-service/main.js
+++ item-service/main.js
@@ -1,3 +1,5 @@
 require("http")
   .createServer((req, res) => res.end("Item service"))
-  .listen(process.env.PORT || 80, err => console.log(err || "Item Service 🖐"));
+  .listen(process.env.PORT || 80, err =>
+    console.log(err || "Peristant change in item service while running within docker-compose")
+  );
